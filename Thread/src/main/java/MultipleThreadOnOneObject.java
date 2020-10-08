0268de9c15839f823b1504000679149c73b21b98
LianxinGao <liamgao017@gmail.com>
2020-10-08 20:31:38 +0800

    update thread

--- Thread/src/main/java/MultipleThreadOnOneObject.java
+++ Thread/src/main/java/MultipleThreadOnOneObject.java
@@ -2,18 +2,27 @@ package main.java;
 
 import sun.misc.MessageUtils;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 // 买火车票为例子, 多个线程操作同一份资源。
 public class MultipleThreadOnOneObject implements Runnable{
     private int ticketNumber = 10;
-
+    private ReentrantLock lock = new ReentrantLock();
     public void run() {
-        while (ticketNumber > 0){
+        while (true){
             try {
-                Thread.sleep(200);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(Thread.currentThread().getName() + " get the " +ticketNumber--+ " ticket!");
+            try {
+                lock.lock();
+                if (ticketNumber > 0){
+                    System.out.println(Thread.currentThread().getName() + " get the " +ticketNumber--+ " ticket!");
+                }else break;
+            }finally {
+                lock.unlock();
+            }
         }
     }
 
