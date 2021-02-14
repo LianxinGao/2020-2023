8f01afe1e37f695c8e1e3cf323a0559cf49ca85a
LianxinGao <liamgao017@gmail.com>
2021-02-14 16:48:11 +0800

    update

---
+++ scala/src/main/scala/oop/layout/Element.scala
@@ -0,0 +1,28 @@
+package oop.layout
+
+abstract class Element {
+  def contents: Array[String]
+
+  val height: Int = contents.length
+  val width: Int = if (height == 0) 0 else contents(0).length
+}
+
+class ArrayElement(override val contents: Array[String]) extends Element {
+
+}
+
+class LineElement(s: String) extends Element {
+  override def contents: Array[String] = Array(s)
+
+  override val height: Int = 1
+  override val width: Int = s.length
+}
+
+class UniformElement(ch: Char,
+                     override val height: Int,
+                     override val width: Int) extends Element {
+  private val line = ch.toString * width
+
+  // fill: (length)(value)
+  override def contents: Array[String] = Array.fill(height)(line)
+}
